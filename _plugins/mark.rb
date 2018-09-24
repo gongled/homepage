28e82e814e8a49867b3e5f058ea506b656b6decf
Gleb Goncharov <inbox@gongled.ru>
2018-09-24 23:50:03 +0400

    Add article about maintaining PACS in the office

--- _plugins/mark.rb
+++ _plugins/mark.rb
@@ -1,15 +1,15 @@
 module Jekyll
     class MarkRenderTag < Liquid::Tag
-  
+
       def initialize(tag_name, text, tokens)
         super
         @text = text
       end
-  
+
       def render(context)
         "<mark>#{@text}</mark>"
       end
     end
   end
-  
+
   Liquid::Template.register_tag('mark', Jekyll::MarkRenderTag)
