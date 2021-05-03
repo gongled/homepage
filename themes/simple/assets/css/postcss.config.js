72a99d3ea3a6c61e52f68bc84fcf02a17c24addd
Gleb Goncharov <inbox@gongled.ru>
2021-05-03 19:36:00 +0400

    Исправить сборку стилей под production

--- themes/simple/assets/css/postcss.config.js
+++ themes/simple/assets/css/postcss.config.js
@@ -1,7 +1,7 @@
 const themeDir = __dirname + '/../../';
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [ './hugo_stats.json' ],
+    content: [ 'hugo_stats.json' ],
     defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
